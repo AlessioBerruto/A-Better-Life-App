affc9dfc3cbcf3c0d1c93b76fe7de7c619170c92
Alessio <alessioberrutoit@gmail.com>
2024-09-10 10:17:34 +0200

    commit prima del deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/A-Better-Life-App/',
+  plugins: [react()],  
 })
